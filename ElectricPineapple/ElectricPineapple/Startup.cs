d99d6ff87e29a799c910c74a8835d3fe7b90cac9
Michael Couture <michaelccouture@gmail.com>
2015-09-30 09:44:53 -0400

    Created Visual Studio project
    with bootstrap layout

---
+++ ElectricPineapple/ElectricPineapple/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElectricPineapple.Startup))]
+namespace ElectricPineapple
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

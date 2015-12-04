ca410aa6d2ae03688999250edeb21023a08205bd
Patrick Bollenbach <pbollenbach-cc@conestogac.on.ca>
2015-12-04 09:31:49 -0500

    Revert "Combined genre and publisher game cards"
    This reverts commit 76d18ee1542f04d14d6fa1e7a1421eef06d02eec.

--- ElectricPineapple/ElectricPineapple/Controllers/PublisherController.cs
+++ ElectricPineapple/ElectricPineapple/Controllers/PublisherController.cs
@@ -18,6 +18,8 @@ namespace ElectricPineapple.Controllers
         // GET: Publisher
         public ActionResult Index()
         {
+            //var publisher = db.Games.Include(a => a.gameID).Include(a => a.publisher);
+            //return View(publisher.ToList());
             return View(db.Publishers.ToList());
         }
 

5ab1ec792021cf4832e1e695e81c2c9e964fc8ff
Michael Couture <mcouture-cc@2A207-B04.conestogac.on.ca>
2015-12-16 09:29:03 -0500

    Added receive promotional emails, select favourite genre

--- ElectricPineapple/ElectricPineapple/Models/ManageViewModels.cs
+++ ElectricPineapple/ElectricPineapple/Models/ManageViewModels.cs
@@ -12,6 +12,7 @@ namespace ElectricPineapple.Models
         public string PhoneNumber { get; set; }
         public bool TwoFactor { get; set; }
         public bool BrowserRemembered { get; set; }
+        public bool ReceivePromotions { get; set; }
     }
 
     public class ManageLoginsViewModel
